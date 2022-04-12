b782612fa86ec0a2ee4367d39ae7c184472a9db6
dirkarnez <smalldirkalex@gmail.com>
2022-04-12 22:09:39 +0800

    - update README.md

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 solver
 ======
-A generic but stupid solver.
+A generic but stupid bruce-force solver. Debuggable in VSCode.
+
+### Intended concepts
+- define a problem (constraints, options, option effect, validation logic)
+
+### TODOs
+- [x] Sorting
+- [ ] Sudoku
+- [ ] Tic-Tac-Toe
--- src/main.cpp
+++ src/main.cpp
@@ -31,20 +31,21 @@
 // }
 
 // 
-// // 7643 7634 7463 7436 7364 7346 6743 6734 6473 6437 6374 6347 4763 4736 4673 4637 4376 4367 3764 3746 3674 3647 3476 3467 
 
 
 #include <iostream>
 #include <vector>
+#include <functional>
 
 using namespace std;
 
-void printPermutn(std::vector<int>* values, std::vector<int>* ans) {
+void printPermutn(std::vector<int>* values, std::vector<int>* ans, std::function<void(std::vector<int>*)> checker) {
     if (values->size() == 0) {
         for (int k = 0; k < ans->size(); k++) {
             cout << ans->at(k);
         }
         cout << " ";
+        checker(ans);
         return;
     }
     
@@ -70,24 +71,63 @@ void printPermutn(std::vector<int>* values, std::vector<int>* ans) {
         new_ans.push_back(value);
         
         // Recursive call
-        printPermutn(&sub_values, &new_ans);
+        printPermutn(&sub_values, &new_ans, checker);
     }
 }
 
 int main()
 {
-    std::vector<int> arr =  { 0, 1, 2 };
-    std::vector<int> temp = { };
+    std::vector<int> arr1 =  { 0, 1, 2 };
+    std::vector<int> temp1 = { };
     
     // 012 021 102 120 201 210 
-    printPermutn(&arr, &temp);
-    
-    cout << endl;
-    
-    //printPermutn("7643", "");
+    printPermutn(&arr1, &temp1, [&](std::vector<int>* current_vector) {
+        for (int i = 0; i < current_vector->size() - 1; i++) {
+            int current = current_vector->at(i);
+            int next = current_vector->at(i + 1);
+            if (current > next) {
+                return;
+            }
+        }
+        cout << "<- sorted!" << " ";
+    });
 
-    std::string str; 
-    
+    cout << endl;
+
+    std::vector<int> arr2 = {7, 6, 4, 3};
+    std::vector<int> temp2 = { };
+
+    // 7643 7634 7463 7436 7364 7346 6743 6734 6473 6437 6374 6347 4763 4736 4673 4637 4376 4367 3764 3746 3674 3647 3476 3467
+    printPermutn(&arr2, &temp2, [&](std::vector<int>* current_vector) {
+        for (int i = 0; i < current_vector->size() - 1; i++) {
+            int current = current_vector->at(i);
+            int next = current_vector->at(i + 1);
+            if (current > next) {
+                return;
+            }
+        }
+        cout << "<- sorted!" << " ";
+    });
+
+    cout << endl;
+
+    std::vector<int> arr3 = {5, 3, 3};
+    std::vector<int> temp3 = { };
+
+    // 533 533 353 335
+    printPermutn(&arr3, &temp3, [&](std::vector<int>* current_vector) {
+        for (int i = 0; i < current_vector->size() - 1; i++) {
+            int current = current_vector->at(i);
+            int next = current_vector->at(i + 1);
+            if (current > next) {
+                return;
+            }
+        }
+        cout << "<- sorted!" << " ";
+    });
+
+
+    std::string str;
     getline(cin, str); 
 
     return 0;
